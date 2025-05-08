8ad92eec5c52a4bac5b419c959aa234ccacec196
Jabors221711 <jaybeebaliling90@gmail.com>
2025-05-08 10:26:56 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')git config --global user.name "Fern922"
+def home():
+    return ("<p>Jabez Cyrus Baliling</p>"
+            "<p>BSIT - 3</p>"
+            "<p>Sytem Integration</p>"
+            "<p>Semi_Final Exam</p>")
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
